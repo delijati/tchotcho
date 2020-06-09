bbc2033718e5fe586d05c6a6e00bfe171065300d
Josip Delic <delijati@googlemail.com>
2020-06-09 22:39:15 +0200

    Bump version: 0.5.1 → 0.5.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tchotcho",
-    version="0.5.1",
+    version="0.5.2",
     author="Josip Delic",
     description="tchotcho",
     long_description=long_description,
